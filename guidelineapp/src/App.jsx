cc8cda22d008f2efa5443be0eb8736801d88b28c
David Kozdra <davidkozdra@gmail.com>
2025-08-29 11:34:57 -0400

    product init

---
+++ guidelineapp/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Product from "./components/product"
+function App() {
+  return (
+    <>
+      <div>
+      
+        Product Grid here 
+
+
+        <Product></Product>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ guidelineapp/src/components/product.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Product() {
+  return (
+    <>
+      <div>
+      
+      </div>
+    </>
+  )
+}
+
+export default Product
