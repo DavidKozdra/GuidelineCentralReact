e58e7eef518ed19a5af5a2d24ecb367a42afaa9c
David Kozdra <davidkozdra@gmail.com>
2025-08-30 11:41:05 -0400

    mark up for filters

--- guidelineapp/src/App.jsx
+++ guidelineapp/src/App.jsx
@@ -2,129 +2,85 @@
 import './App.css'
 import Product from "./components/product"
 function App() {
-  let items =[
+  //product_type, gender and material
+  let items = [
     {
-      name:"shirt", description:"a fine t shirt red and green stripes",price:7, moreDetails:"added details including scale of the item and location sourced"
-    }, 
+      name: "Trex shirt", description: "a fine t shirt red and green stripes", price: 7, moreDetails: "added details including scale of the item and location sourced", product_type: "shirt", gender: "male", material: "silk"
+    },
 
     {
-      name:"lawn care kit", description:"fertilizer and even some treatment for color",price:20, moreDetails:"added details including scale of the item and location sourced"
-    }, 
+      name: "pink hoodie", description: "pink hoodie", price: 20, moreDetails: "added details including scale of the item and location sourced", product_type: "hoodie", gender: "female", material: "polyester"
+    },
 
     {
-      name:"Remote", description:"a great remote for any TV",price:15, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-      {
-      name:"shirt", description:"a fine t shirt red and green stripes",price:7, moreDetails:"added details including scale of the item and location sourced"
-    }, 
+      name: "chrismas sweater", description: "sweater with a big green tree", price: 15, moreDetails: "added details including scale of the item and location sourced", product_type: "sweater", gender: "unisex", material: "cotton"
+    },
+    {
+      name: "Blue shirt", description: "Simple blue colared shirt", price: 7, moreDetails: "added details including scale of the item and location sourced", product_type: "shirt", gender: "male", material: "silk"
+    },
 
     {
-      name:"lawn care kit", description:"fertilizer and even some treatment for color",price:20, moreDetails:"added details including scale of the item and location sourced"
-    }, 
+      name: "Purple hoodie", description: "Nice purple hoodie for a lady", price: 20, moreDetails: "added details including scale of the item and location sourced", product_type: "hoodie", gender: "female", material: "polyester"
+    },
 
     {
-      name:"Remote", description:"a great remote for any TV",price:15, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-      {
-      name:"shirt", description:"a fine t shirt red and green stripes",price:7, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"lawn care kit", description:"fertilizer and even some treatment for color",price:20, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"Remote", description:"a great remote for any TV",price:15, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-         {
-      name:"shirt", description:"a fine t shirt red and green stripes",price:7, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"lawn care kit", description:"fertilizer and even some treatment for color",price:20, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"Remote", description:"a great remote for any TV",price:15, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-      {
-      name:"shirt", description:"a fine t shirt red and green stripes",price:7, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"lawn care kit", description:"fertilizer and even some treatment for color",price:20, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"Remote", description:"a great remote for any TV",price:15, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-         {
-      name:"shirt", description:"a fine t shirt red and green stripes",price:7, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"lawn care kit", description:"fertilizer and even some treatment for color",price:20, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"Remote", description:"a great remote for any TV",price:15, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-      {
-      name:"shirt", description:"a fine t shirt red and green stripes",price:7, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"lawn care kit", description:"fertilizer and even some treatment for color",price:20, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"Remote", description:"a great remote for any TV",price:15, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-         {
-      name:"shirt", description:"a fine t shirt red and green stripes",price:7, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"lawn care kit", description:"fertilizer and even some treatment for color",price:20, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"Remote", description:"a great remote for any TV",price:15, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-      {
-      name:"shirt", description:"a fine t shirt red and green stripes",price:7, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"lawn care kit", description:"fertilizer and even some treatment for color",price:20, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
-    {
-      name:"Remote", description:"a great remote for any TV",price:15, moreDetails:"added details including scale of the item and location sourced"
-    }, 
-
+      name: "chrismas sweater", description: "a great remote for any TV", price: 15, moreDetails: "added details including scale of the item and location sourced", product_type: "sweater", gender: "unisex", material: "cotton"
+    },
   ]
 
   return (
     <>
-      <div>
-      
-                  <div className='grid' >
-        {
-          items.map((item,i) => {
-              
+      <div className='main-container'>
+
+        <div className='filter-container'>
+          <h1>
+            Options
+          </h1>
+
+          <select title='product Type' >
+            <option>shirt</option>
+
+            <option>hoodie</option>
+
+            <option>sweater</option>
+          </select>
+
+
+           <select title='Gender' >
+            <option>male</option>
+
+            <option>female</option>
+
+            <option>unisex</option>
+          </select>
+
+
+           <select title='material' >
+            <option>silk</option>
+
+            <option>polyester</option>
+
+            <option>cotton</option>
+          </select>
+        </div>
+
+
+        <div className='grid' >
+          {
+            items.map((item, i) => {
+
 
               return (
 
-                    <Product name={item.name} description={item.description}  price={item.price}  imageURL="https://placehold.co/400x200"  moreDetails={item.moreDetails} key={i}>
-                    </Product>
-            
-                )
+                <Product name={item.name} description={item.description} price={item.price} imageURL="https://placehold.co/400x200" moreDetails={item.moreDetails} key={i}>
+                </Product>
 
-          })
-          
-        }
-              </div>
+              )
+
+            })
+
+          }
+        </div>
 
 
 
